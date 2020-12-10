f9cae58b8f38fb37c1dccb5e86ed760084b3bc98
xuezhongkai <15824892975@163.com>
2020-12-10 18:34:46 +0800

    .

---
+++ src/assets/js/componentConfig/config/collapse/index.js
@@ -0,0 +1,11 @@
+import collapse from './collapse.js';
+
+export default [
+  {
+    name: '折叠面板',
+    icon: 'el-icon-s-unfold',
+    content: '添加折叠面板',
+    value: 'collapse',
+    data: collapse
+  }
+]
---
+++ src/components/widgetPage/widgetList/components/wgCollapse.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="wg-collapse" :style="{ backgroundColor: item.style.backgroundColor }">
+    <div 
+      v-for="(collapseItem, index) in item.sonlist"
+      :key="index"
+      :style="{ marginTop: item.style.parentMargin + 'px'}"
+      class="collapse-box"
+    >
+      <!-- :style="{ marginTop: index !== 0? item.style.parentMargin + 'px': 'none'}" -->
+      <img 
+        class="collapse-img" 
+        :src="collapseItem.img" 
+        :style="{ height: item.style.height + 'px' }" 
+        alt=""
+        @click="showSon(index)"
+      />
+      <div 
+        v-for="(sonItem, sonIndex) in collapseItem.list"
+        v-show="selectIndex === index"
+        :key="sonIndex"
+        class="collapse-son-box"
+      >
+        <img 
+          class="collapse-son-img" 
+          :src="sonItem.img" 
+          :style="{
+            height: item.style.sonHeight + 'px',
+            marginTop: item.style.sonMargin + 'px'
+          }" 
+          alt=""
+          @click="$toPatch(sonItem.link)"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'wgCollapse',
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      selectIndex: null
+    }
+  },
+  methods: {
+    showSon (index) {
+      this.selectIndex = this.selectIndex !== index ? index : null
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .wg-collapse{
+    width: 100%;
+    .collapse-box{
+      width: 100%;
+      .collapse-img{
+        display: block;
+        width: 100%;
+      }
+      .collapse-son-box{
+        width: 100%;
+        .collapse-son-img{
+          display: block;
+          width: 100%;
+        }
+      }
+    }
+  }
+</style>
